2d942069e81a78c1fa02877a672043165f8d36b9
Gian Levi Noche <gian.noche@gmail.com>
2024-07-19 18:13:50 +0200

    Fix carousel

--- script.js
+++ script.js
@@ -3,12 +3,14 @@ const next = document.querySelector(".next");
 
 const track = document.querySelector(".track");
 
-const carouselWidth = document.querySelector(".carousel-container").offsetWidth;
+const carouselWidth = document.querySelector(".card-container").offsetWidth;
+
+console.log(carouselWidth)
 
 next.addEventListener("click", () => {
-  track.style.transform = "translateX(-${carouselWidth}px)";
+  track.style.transform = `translateX(-${carouselWidth}px)`;
 });
 
 prev.addEventListener("click", () => {
-  track.style.transform = "translateX(-${0}px)";
+  track.style.transform = `translateX(-${0}px)`;
 });
